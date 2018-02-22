6d5094c6c029424a07dca6ec0ff981ad5ac2b44d
alexander_ketov <alexander_ketov@epam.com>
2018-02-22 13:00:18 +0600

    Implement Node class for future Tree implementation.

--- tree/index.js
+++ tree/index.js
@@ -1,4 +1,16 @@
 class Node {
+    constructor(data, children = []) {
+        this.data = data;
+        this.children = children;
+    }
+    
+    add(data) {
+        this.children.push(new Node(data));
+    }
+    
+    remove(data) {
+        this.children = this.children.filter(node => node.data !== data);
+    }
 }
 
 class Tree {
