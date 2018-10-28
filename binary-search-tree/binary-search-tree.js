96c5967c2f48ae0d81e4a551114892dcd1d6f1bb
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-10-28 19:57:51 +0600

    Minor changes of BST implementation.

--- binary-search-tree/binary-search-tree.js
+++ binary-search-tree/binary-search-tree.js
@@ -45,6 +45,155 @@ class BinarySearchTree {
     }
 
     /**
+     * Removing the node from the Tree by data
+     * @param data
+     */
+    remove(data) {
+        const removeNode = function (node, data) {
+            if (node === null) {
+                return null;
+            }
+
+            if (data === node.data) {
+                // node has no children
+                if (node.left === null && node.right === null) {
+                    return null;
+                }
+                // node has no left child
+                if (node.left === null) {
+                    return node.right;
+                }
+                // if node has no right child
+                if (node.right === null) {
+                    return node.left;
+                }
+                // node has two children
+                let tempNode = node.right;
+                while(tempNode.left !== null) {
+                    tempNode = tempNode.left;
+                }
+                node.data = tempNode.data;
+                node.right = removeNode(node.right, tempNode.data);
+                return node;
+            } else if (data < node.data) {
+                node.left = removeNode(node.left, data);
+                return node;
+            } else {
+                node.right = removeNode(node.right, data);
+                return node;
+            }
+        };
+        this.root = removeNode(this.root, data);
+    }
+
+    /**
+     * Traversing the tree in order
+     * That means that the traversal will visit all of the nodes
+     * of a BST in ascending order of the node key values
+     * Depth-First Traversal
+     * @returns {*}
+     */
+    inOrder() {
+        if (this.root === null) {
+            return null;
+        } else {
+            let result = [];
+
+            const traverseInOrder = node => {
+                node.left && traverseInOrder(node.left);
+                result.push(node.data);
+                node.right && traverseInOrder(node.right);
+            };
+
+            traverseInOrder(this.root);
+
+            return result;
+        }
+    }
+
+    /**
+     * Traversing the tree in pre order
+     * That means that the traversal will visit the root node first,
+     * followed by the nodes in the subtrees under the left child of the root node,
+     * followed by the nodes in the subtrees under the right child of the root node.
+     * Depth-First Traversal
+     * @returns {*}
+     */
+    preOrder() {
+        if (this.root === null) {
+            return null;
+        } else {
+            let result = [];
+
+            const traversePreOrder = node => {
+                result.push(node.data);
+                node.left && traversePreOrder(node.left);
+                node.right && traversePreOrder(node.right);
+            };
+
+            traversePreOrder(this.root);
+
+            return result;
+        }
+    }
+
+    /**
+     * Traversing the tree in post order
+     * That means that the traversal will visit all of the child nodes
+     * of the left subtree up to the root node,
+     * and then will visit all of the child nodes of the right subtree
+     * up to the root node
+     * Depth-First Traversal
+     * @returns {*}
+     */
+    postOrder() {
+        if (this.root === null) {
+            return null;
+        } else {
+            let result = [];
+
+            const traversePreOrder = node => {
+                node.left && traversePreOrder(node.left);
+                node.right && traversePreOrder(node.right);
+                result.push(node.data);
+            };
+
+            traversePreOrder(this.root);
+
+            return result;
+        }
+    }
+
+    /**
+     * Level-order traversal
+     * Breadth-First Traversal
+     * @returns {*}
+     */
+    levelOrder() {
+        let result = [];
+        let Q = [];
+
+        if (this.root !== null) {
+            Q.push(this.root);
+
+            while (Q.length > 0) {
+                let node = Q.shift();
+                result.push(node.data);
+                if (node.left !== null) {
+                    Q.push(node.left);
+                }
+                if (node.right !== null) {
+                    Q.push(node.right);
+                }
+            }
+
+            return result;
+        } else {
+            return null;
+        }
+    }
+
+    /**
      * Searching the minimum value
      * @returns {*}
      */
@@ -118,48 +267,6 @@ class BinarySearchTree {
     }
 
     /**
-     * Removing the node from the Tree by data
-     * @param data
-     */
-    remove(data) {
-        const removeNode = function (node, data) {
-            if (node === null) {
-                return null;
-            }
-
-            if (data === node.data) {
-                // node has no children
-                if (node.left === null && node.right === null) {
-                    return null;
-                }
-                // node has no left child
-                if (node.left === null) {
-                    return node.right;
-                }
-                // if node has no right child
-                if (node.right === null) {
-                    return node.left;
-                }
-                // node has two children
-                let tempNode = node.right;
-                while(tempNode.left !== null) {
-                    tempNode = tempNode.left;
-                }
-                node.data = tempNode.data;
-                node.right = removeNode(node.right, tempNode.data);
-                return node;
-            } else if (data < node.data) {
-                node.left = removeNode(node.left, data);
-                return node;
-            } else {
-                node.right = removeNode(node.right, data);
-                return node;
-            }
-        };
-        this.root = removeNode(this.root, data);
-    }
-
-    /**
      * Checking if the Tree is balanced or not
      *
      * The Tree is balanced when difference between max and min heights is 1 at most
@@ -204,110 +311,6 @@ class BinarySearchTree {
             return left + 1;
         } else {
             return right + 1;
-        }
-    }
-
-    /**
-     * Traversing the tree in order
-     * That means that the traversal will visit all of the nodes
-     * of a BST in ascending order of the node key values
-     * @returns {*}
-     */
-    inOrder() {
-        if (this.root === null) {
-            return null;
-        } else {
-            let result = [];
-
-            const traverseInOrder = node => {
-                node.left && traverseInOrder(node.left);
-                result.push(node.data);
-                node.right && traverseInOrder(node.right);
-            };
-
-            traverseInOrder(this.root);
-
-            return result;
-        }
-
-        // if (!(node === null)) {
-        //     this.inOrder(node.left);
-        //     this.inOrder(node.right);
-        // }
-    }
-
-    /**
-     * Traversing the tree in pre order
-     * That means that the traversal will visit the root node first,
-     * followed by the nodes in the subtrees under the left child of the root node,
-     * followed by the nodes in the subtrees under the right child of the root node.
-     * @returns {*}
-     */
-    preOrder() {
-        if (this.root === null) {
-            return null;
-        } else {
-            let result = [];
-
-            const traversePreOrder = node => {
-                result.push(node.data);
-                node.left && traversePreOrder(node.left);
-                node.right && traversePreOrder(node.right);
-            };
-
-            traversePreOrder(this.root);
-
-            return result;
-        }
-    }
-
-    /**
-     * Traversing the tree in post order
-     * That means that the traversal will visit all of the child nodes
-     * of the left subtree up to the root node,
-     * and then will visit all of the child nodes of the right subtree
-     * up to the root node
-     * @returns {*}
-     */
-    postOrder() {
-        if (this.root === null) {
-            return null;
-        } else {
-            let result = [];
-
-            const traversePreOrder = node => {
-                node.left && traversePreOrder(node.left);
-                node.right && traversePreOrder(node.right);
-                result.push(node.data);
-            };
-
-            traversePreOrder(this.root);
-
-            return result;
-        }
-    }
-
-    levelOrder() {
-        let result = [];
-        let Q = [];
-
-        if (this.root !== null) {
-            Q.push(this.root);
-
-            while (Q.length > 0) {
-                let node = Q.shift();
-                result.push(node.data);
-                if (node.left !== null) {
-                    Q.push(node.left);
-                }
-                if (node.right !== null) {
-                    Q.push(node.right);
-                }
-            }
-
-            return result;
-        } else {
-            return null;
         }
     }
 }
